bba8fd43483350562e3352614102446c0d5621c4
Coppertine <nick_bishop1@outlook.com>
2019-09-02 11:54:42 +1000

    Including objects into controller

--- src/com/coppertine/tafe/java/ITTownTrafficManager/MonitorOffice/ITTownMonitorOfficeController.java
+++ src/com/coppertine/tafe/java/ITTownTrafficManager/MonitorOffice/ITTownMonitorOfficeController.java
@@ -5,13 +5,19 @@
  */
 package com.coppertine.tafe.java.ITTownTrafficManager.MonitorOffice;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -23,7 +29,59 @@ import javafx.stage.Stage;
 public class ITTownMonitorOfficeController implements Initializable {    
     double x;
     double y;
-    public TextArea txtLinkedList;
+    
+    /**
+     * Interactable and changable Objects used into the FXML.
+     */
+
+    /** Linked List Text Area. */
+    @FXML
+    private TextArea txtLinkedList; // Linked List Text Area
+
+    /** Window Menubar. */
+    @FXML
+    private MenuBar windowMenuBar;
+    /** Menu objects. */
+    @FXML
+    private Menu fileMenu;  /** File Menu. */
+    @FXML
+    private Menu editMenu;  /** Edit Menu. */
+    @FXML
+    private Menu helpMenu;  /** Help Menu. */
+
+    /** FontAwesomeIcons. */
+    /** Minimise Icon. */
+    @FXML
+    private FontAwesomeIconView minimiseIcon;
+    /** Resize Icon. */
+    @FXML
+    private FontAwesomeIconView resizeIcon;
+    @FXML
+    private FontAwesomeIconView closeIcon;
+
+    /** Server Buttons. */
+    /** Start Server. */
+    @FXML
+    private MenuItem mItemStartServer;
+
+    /** Server Options */
+    @FXML
+    private MenuItem mItemServerOptions;
+
+    /** Table */
+    @FXML
+    private TableColumn<?, ?> tblHeadder;
+    @FXML
+    private TableColumn<?, ?> tblTrafficTime;
+    @FXML
+    private TableColumn<?, ?> tblTrafficLocation;
+    @FXML
+    private TableColumn<?, ?> tblTrafficAverageVeh;
+    @FXML
+    private TableColumn<?, ?> tblTrafficAverageVel;
+    
+    @FXML
+    private Button sort
     /**
      * Drags window on mouse held.
      * @param event MouseEvent.
@@ -63,4 +121,130 @@ public class ITTownMonitorOfficeController implements Initializable {
         
     }
     
+    @FXML
+    public void sortBY
+
+    //<editor-fold defaultstate="collasped" desc="Getters and Setters">
+    public TextArea getTxtLinkedList() {
+        return txtLinkedList;
+    }
+
+    public void setTxtLinkedList(TextArea txtLinkedList) {
+        this.txtLinkedList = txtLinkedList;
+    }
+
+    public MenuBar getWindowMenuBar() {
+        return windowMenuBar;
+    }
+
+    public void setWindowMenuBar(MenuBar windowMenuBar) {
+        this.windowMenuBar = windowMenuBar;
+    }
+
+    public Menu getFileMenu() {
+        return fileMenu;
+    }
+
+    public void setFileMenu(Menu fileMenu) {
+        this.fileMenu = fileMenu;
+    }
+
+    public Menu getEditMenu() {
+        return editMenu;
+    }
+
+    public void setEditMenu(Menu editMenu) {
+        this.editMenu = editMenu;
+    }
+
+    public Menu getHelpMenu() {
+        return helpMenu;
+    }
+
+    public void setHelpMenu(Menu helpMenu) {
+        this.helpMenu = helpMenu;
+    }
+
+    public FontAwesomeIconView getMinimiseIcon() {
+        return minimiseIcon;
+    }
+
+    public void setMinimiseIcon(FontAwesomeIconView minimiseIcon) {
+        this.minimiseIcon = minimiseIcon;
+    }
+
+    public FontAwesomeIconView getResizeIcon() {
+        return resizeIcon;
+    }
+
+    public void setResizeIcon(FontAwesomeIconView resizeIcon) {
+        this.resizeIcon = resizeIcon;
+    }
+
+    public FontAwesomeIconView getCloseIcon() {
+        return closeIcon;
+    }
+
+    public void setCloseIcon(FontAwesomeIconView closeIcon) {
+        this.closeIcon = closeIcon;
+    }
+
+    public MenuItem getmItemStartServer() {
+        return mItemStartServer;
+    }
+
+    public void setmItemStartServer(MenuItem mItemStartServer) {
+        this.mItemStartServer = mItemStartServer;
+    }
+
+    public MenuItem getmItemServerOptions() {
+        return mItemServerOptions;
+    }
+
+    public void setmItemServerOptions(MenuItem mItemServerOptions) {
+        this.mItemServerOptions = mItemServerOptions;
+    }
+
+    public TableColumn<?, ?> getTableHeader() {
+        return TableHeader;
+    }
+
+    public void setTableHeader(TableColumn<?, ?> TableHeader) {
+        this.TableHeader = TableHeader;
+    }
+
+    public TableColumn<?, ?> getTrafficTime() {
+        return TrafficTime;
+    }
+
+    public void setTrafficTime(TableColumn<?, ?> TrafficTime) {
+        this.TrafficTime = TrafficTime;
+    }
+
+    public TableColumn<?, ?> getTrafficLocation() {
+        return TrafficLocation;
+    }
+
+    public void setTrafficLocation(TableColumn<?, ?> TrafficLocation) {
+        this.TrafficLocation = TrafficLocation;
+    }
+
+    public TableColumn<?, ?> getTrafficAverageVeh() {
+        return TrafficAverageVeh;
+    }
+
+    public void setTrafficAverageVeh(TableColumn<?, ?> TrafficAverageVeh) {
+        this.TrafficAverageVeh = TrafficAverageVeh;
+    }
+
+    public TableColumn<?, ?> getTrafficAverageVel() {
+        return TrafficAverageVel;
+    }
+
+    public void setTrafficAverageVel(TableColumn<?, ?> TrafficAverageVel) {
+        this.TrafficAverageVel = TrafficAverageVel;
+    }
+//</editor-fold>
+
+
 }
