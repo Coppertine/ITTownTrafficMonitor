318696ac39685a19fe1a75fb61428fdaaaa9f126
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-10-10 09:34:58 +1000

    Client properties

--- src/com/coppertine/tafe/java/ITTownTrafficManager/ClientStation/TrafficClient.java
+++ src/com/coppertine/tafe/java/ITTownTrafficManager/ClientStation/TrafficClient.java
@@ -36,7 +36,7 @@ public class TrafficClient implements Runnable {
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
